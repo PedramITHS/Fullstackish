6410f2481e1a5cf6307679af288c27b80733ae6d
Pedram <pedram.hejazi.kenari@iths.se>
2024-10-28 22:31:01 +0100

    New files added, backend/frontend changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./pages/App.jsx";
+import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
-import Footer from "./components/footer.jsx";
 import "./css/index.css";
 
 createRoot(document.getElementById("root")).render(
